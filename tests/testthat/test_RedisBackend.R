e66d99a81ec8f0dc71d77cde61cc4f838695431d
Martin Morgan <Martin.Morgan@RoswellPark.org>
2021-07-26 13:08:51 -0400

    revise unit tests for clarity / modularity
    - each test_that is independent
    - avoid confusing `expect_error(..., NA)` when no error expected
    - use BiocParallel::ipcid() for job name, for greater independence

--- tests/testthat/test_RedisBackend.R
+++ tests/testthat/test_RedisBackend.R
@@ -1,45 +1,72 @@
-skip_if_not(rpalive(RedisParam(1L)))
+test_that("Creating RedisBackend succeeds", {
+    skip_if_not(rpalive())
 
-jobname <- "test_RedisBackend"
-manager <- NULL
-worker1 <- NULL
-worker2 <- NULL
+    jobname <- BiocParallel::ipcid()
+    expect_true(validObject(RedisBackend(jobname = jobname, type = "manager")))
+    expect_true(validObject(RedisBackend(jobname = jobname, type = "worker")))
 
-test_that("Creating RedisBackend", {
-    expect_error(manager <<- RedisBackend(jobname = jobname, type = "manager"), NA)
-    expect_error(worker1 <<- RedisBackend(jobname = jobname, type = "worker"), NA)
-    expect_error(worker2 <<- RedisBackend(jobname = jobname, type = "worker"), NA)
+    gc() # clean-up manager & worker
 })
 
-test_that("Check workers", {
+test_that("Managers and workers start and end correctly", {
+    skip_if_not(rpalive())
+
+    jobname <- BiocParallel::ipcid()
+    manager <- RedisBackend(jobname = jobname, type = "manager")
+    workers <- replicate(2L, RedisBackend(jobname = jobname, type = "worker"))
+
     expect_equal(length(manager), 2L)
-    expect_true(
-        setequal(
-            bpworkers(manager),
-            c(worker1$id,worker2$id)
-        )
-    )
+    worker_ids <- vapply(workers, `[[`, character(1), "id")
+    expect_true(setequal(bpworkers(manager), worker_ids))
+
+    result <- lapply(workers, .quit)
+    expect_identical(result, rep(list(NULL), 2))
+    expect_null(.quit(manager))
 })
 
 test_that("job dispatching function", {
+    skip_if_not(rpalive())
+
+    jobname <- BiocParallel::ipcid()
+    manager <- RedisBackend(jobname = jobname, type = "manager")
+    worker <- RedisBackend(jobname = jobname, type = "worker")
+
     jobValue <- "job message"
+    expect_true(.send_to(manager, 1, jobValue))
+    expect_equal(.recv(worker), jobValue)
+
     resultValue <- "result message"
-    expect_error(.send_to(manager, 1, jobValue), NA)
-    expect_equal(.recv(worker1), jobValue)
-    expect_error(.pushResult(worker1, resultValue), NA)
+    expect_equal(.pushResult(worker, resultValue), 1L)
     expect_equal(.popResult(manager), resultValue)
+
+    expect_null(.quit(worker))
+    expect_null(.quit(manager))
 })
 
 test_that("Job management", {
+    skip_if_not(rpalive())
+
+    jobname <- BiocParallel::ipcid()
+    manager <- RedisBackend(jobname = jobname, type = "manager")
+    workers <- replicate(2L, RedisBackend(jobname = jobname, type = "worker"))
+
     jobValue <- "job message"
+    expect_true(.send_to(manager, 1, jobValue))
+    expect_equal(.recv(workers[[1]]), jobValue)
+
+    ## kill worker 1
+    .quit(workers[[1]])
+    result <-.resubmitMissingJobs(manager)
+
+    ## Receive the job from worker 2
+    expect_equal(.recv(workers[[2]]), jobValue)
+
+    ## return result to manager
     resultValue <- "result message"
-    expect_error(.send_to(manager, 1, jobValue), NA)
-    expect_equal(.recv(worker1), jobValue)
-    ## kill worker1
-    .close(worker1)
-    .resubmitMissingJobs(manager)
-    ## Receive the job from worker2
-    expect_equal(.recv(worker2), jobValue)
-    expect_error(.pushResult(worker2, resultValue), NA)
+    expect_identical(.pushResult(workers[[2]], resultValue), 1L)
     expect_equal(.popResult(manager), resultValue)
+
+    .quit(manager)
+    expect_error(.quit(workers[[1]])) # already closed
+    .quit(workers[[2]])
 })
