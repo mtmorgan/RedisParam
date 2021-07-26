74678efbdef40d1df407b49ff16e1d2e49633eda
Jiefei Wang <szwjf08@gmail.com>
2021-07-26 18:03:24 +0800

    fix an issue caused by asynchronous redis commands in manager and worker

--- inst/LuaScript/resubmit_missing_tasks.lua
+++ inst/LuaScript/resubmit_missing_tasks.lua
@@ -7,23 +7,26 @@ local waitingTask = redis.call("SMEMBERS", managerTaskSet)
 local publicTasks = nil
 for i = 1, #waitingTask, 1 do
     local taskId = waitingTask[i]
-    local workerId = redis.call("lrange", taskId, 2, 2)[1]
-    local missing = false
-    if (workerId ~= "public") and (not existsElement(liveWorkers, workerId)) then
-        missing = true
-    end
-    if workerId == "public" then
-        if publicTasks == nil then
-            publicTasks = redis.call("lrange", publicTaskQueue, 0, -1)
-        end
-        if not existsElement(publicTasks, taskId) then
+    local existsTask = redis.call("exists", taskId)
+    if existsTask == 1 then
+        local workerId = redis.call("lrange", taskId, 2, 2)[1]
+        local missing = false
+        if (workerId ~= "public") and (not existsElement(liveWorkers, workerId)) then
             missing = true
         end
-    end
-    if missing then
-        table.insert(missingWorker, workerId)
-        redis.call('lset', taskId, 2, "public")
-        redis.call("rpush", publicTaskQueue, taskId)
+        if workerId == "public" then
+            if publicTasks == nil then
+                publicTasks = redis.call("lrange", publicTaskQueue, 0, -1)
+            end
+            if not existsElement(publicTasks, taskId) then
+                missing = true
+            end
+        end
+        if missing then
+            table.insert(missingWorker, workerId)
+            redis.call('lset', taskId, 2, "public")
+            redis.call("rpush", publicTaskQueue, taskId)
+        end
     end
 end
 
